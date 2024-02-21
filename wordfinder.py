07f3966107676af4324654d0aa477a559452a76c
Samuel Myung <shmyung4@gmail.com>
2024-02-20 17:13:17 -0800

    Completed the WordFinder class

--- wordfinder.py
+++ wordfinder.py
@@ -1,20 +1,20 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
     def __init__(self, file_path):
-        # call another function to generate words
+        '''Create a list of words from a text file'''
         self.words = self.get_words(file_path)
 
-
-
     def random(self):
-        ''' Generates a random word from the self.words list '''
-        pass
+        '''Generates a random word from the self.words list'''
+        return choice(self.words)
 
     def get_words(self, file_path):
-        ''' Opens and reads a file of words and returns a list of them'''
+        '''Opens and reads a file of words, splitting on new lines, and returns a list of them'''
         file = open(file_path)
 
-        word_list = [line[0:-2] for line in file]
+        word_list = [line.strip() for line in file]
 
         file.close()
 
