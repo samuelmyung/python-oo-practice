49023d292fe7a2b4196252a3c873792191cdc65f
James Lee <jimhlee23@gmail.com>
2024-02-20 21:07:06 -0500

    completed special word finder

--- wordfinder.py
+++ wordfinder.py
@@ -1,9 +1,9 @@
 from random import choice
 
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    '''Word Finder: finds random words from a text file'''
     def __init__(self, file_path):
-        '''Create a list of words from a text file'''
+        '''Create a list of words from a filepath'''
         self.words = self.get_words(file_path)
 
     def random(self):
@@ -21,3 +21,27 @@ class WordFinder:
         return word_list
 
 
+
+class SpecialWordFinder(WordFinder):
+    '''Special Word Finder: finds random words from a text file but ignores comments and whitespace'''
+    # Don't need init either
+    # def __init__(self, file_path):
+    #     '''Create a list of words from a filepath'''
+    #     super().__init__(file_path)
+    # Python will look for random in the parent if the child doesn't have it. No need to repeat.
+    # def random(self):
+    #     '''Generates a random word from the self.words list'''
+
+    #     return super().random()
+
+    def get_words(self, file_path):
+        '''Opens and reads a file of words and returns a list of them. Filters out
+        whitespace, comments and new line characters'''
+        # can take from parent
+        words_from_parent = super().get_words(file_path)
+        # file = open(file_path)
+
+        # word_list = [line.strip() for line in file if line[0] != '#' and bool(line.strip())]
+        word_list = [word for word in words_from_parent if word and word[0] != '#']
+
+        return word_list
