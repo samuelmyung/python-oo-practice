e07ed3ac284b4535b16d91993e3c7db78239c0d8
James Lee <jimhlee23@gmail.com>
2024-02-20 19:14:30 -0500

    finished get_words

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,23 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+    def __init__(self, file_path):
+        # call another function to generate words
+        self.words = self.get_words(file_path)
+
+
+
+    def random(self):
+        ''' Generates a random word from the self.words list '''
+        pass
+
+    def get_words(self, file_path):
+        ''' Opens and reads a file of words and returns a list of them'''
+        file = open(file_path)
+
+        word_list = [line[0:-2] for line in file]
+
+        file.close()
+
+        return word_list
+
+
